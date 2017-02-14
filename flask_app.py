ca27e3511d68145494c9e8297be2053802296da3
Billy Gallagher <bjg222@gmail.com>
2017-02-14 02:50:17 +0000

    Actually works

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
 app.secret_key = 'This is really unique and secret'
 app.config["DEBUG"] = True
 
-@app.route('/')
+comments = []
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("main_page.html")
+    if request.method == "GET":
+        return render_template("main_page.html", comments=comments)
+    comments.append(request.form["contents"])
+    return redirect(url_for("index"))
