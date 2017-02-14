e37e41e2f85442bc182c33be5c5e63a74ace7b45
Billy Gallagher <bjg222@gmail.com>
2017-02-14 03:19:20 +0000

    DB Setup

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, redirect, request, url_for
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = 'This is really unique and secret'
 app.config["DEBUG"] = True
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username="bjg222",
+    password="XD5i%z@7Z8LK",
+    hostname="bjg222.mysql.pythonanywhere-services.com",
+    databasename="bjg222$comments",
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+
+db = SQLAlchemy(app)
 
 comments = []
 
