e03f1ed0a0bc57c2591602ba7d1b22722d44bb31
Billy Gallagher <bjg222@gmail.com>
2017-12-20 10:55:22 -0500

    python anywhere has an old version of flask_wtf, added code to handle changes in import

--- bandb/main/forms.py
+++ bandb/main/forms.py
@@ -3,8 +3,10 @@ Created on May 3, 2017
 
 @author: wjgallag
 '''
-
-from flask_wtf import FlaskForm
+try:
+    from flask_wtf import FlaskForm
+except ImportError:
+    from flask_wtf import From as FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import InputRequired, Email
